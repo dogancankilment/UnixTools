17526679216e92028080763aa99f83a99c440bf6
dogancankilment <dogancankilment@gmail.com>
2014-08-08 09:38:55 +0300

    syntax configured to function syntax

--- More-Python/Activation_Key_Generator.py
+++ More-Python/Activation_Key_Generator.py
@@ -1,20 +1,24 @@
-"""
-Activation key -> generated from combination
-email and random salt SHA1 hash
-
-"""
-
 import hashlib
 import random
 
 
-salt = hashlib.sha1(str(random.random())).hexdigest()[:10]
-email = "dogancankilment@gmail.com"
+def activation_key_generator():
+    """
+    Activation key -> generated from combination
+    email and random salt SHA1 hash
 
-if isinstance(email, unicode):
-    email = email.encode('utf-8')
-activation_key = hashlib.sha1(salt+email).hexdigest()
+    """
 
-print "hashlib-salt: \n", salt
-print "email: \n", email
-print "activation_key: \n", activation_key
+    salt = hashlib.sha1(str(random.random())).hexdigest()[:10]
+    email = "dogancankilment@gmail.com"
+
+    if isinstance(email, unicode):
+        email = email.encode('utf-8')
+    activation_key = hashlib.sha1(salt+email).hexdigest()
+
+    print "hashlib-salt: \n", salt
+    print "email: \n", email
+    print "activation_key: \n", activation_key
+
+if __name__ == '__main__':
+    activation_key_generator()
