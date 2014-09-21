bcae7073bcfe6d9b004e4ca8b1b4c6a5b4c8fc15
dogancankilment <dogancankilment@gmail.com>
2014-09-22 01:53:30 +0300

    home page created

--- showcase/index/views.py
+++ showcase/index/views.py
@@ -1,1 +1,7 @@
-# Create your views here.
+from django.shortcuts import redirect, render_to_response, get_object_or_404, render
+# from django.template import RequestContext
+from django.core.urlresolvers import reverse
+
+
+def home(request):
+    return render(request, 'index/home.html')
