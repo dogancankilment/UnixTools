563ba2e8d54abc0ca574234c1accbde46abea61f
dogancankilment <dogancankilment@gmail.com>
2014-09-22 01:36:39 +0300

    showcase urls edited for apps,homepage and admin

--- showcase/showcase/urls.py
+++ showcase/showcase/urls.py
@@ -1,17 +1,19 @@
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'showcase.views.home', name='home'),
-    # url(r'^showcase/', include('showcase.foo.urls')),
+                       url(r'^admin/',
+                           include(admin.site.urls)),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+                       url(r'^$',
+                           'index.views.home',
+                           name='home'),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+                       url(r'^unixtools/',
+                           include('index.urls')),
+
 )
+
+handler404 = 'index.views.my_custom_404'
