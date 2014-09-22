18c86407d58ed75abd1a0b928c458dddc89c8206
dogancankilment <dogancankilment@gmail.com>
2014-09-22 06:31:38 +0300

    home view wrote

--- showcase/index/views.py
+++ showcase/index/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import redirect, render_to_response, get_object_or_404, render
 # from django.template import RequestContext
 from django.core.urlresolvers import reverse
+from utils.bash_commands import command_entry
+from forms import CommandForm
 
 
 def home(request):
-    return render(request, 'index/home.html')
+    result = ""
+    form = CommandForm()
+    if request.POST or None:
+        form = CommandForm(request.POST)
+        if form.is_valid():
+            entry = form.cleaned_data['entry']
+            result = command_entry(entry)
+
+    return render(request,
+                  'index/home.html',
+                  {'form':form,
+                   'result':result})
